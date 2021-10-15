554f5f43e6eda2ab2421bdd01ab2d27ed571f56f
dasoncheng <dasoncheng@outlook.com>
2021-10-15 13:11:00 +0800

    feat: 案例grpc编写调试，配置更新

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+import "reflect-metadata";
+import { Server, ServerCredentials } from "@grpc/grpc-js";
+import services from "../packages/grpc/greeter_grpc_pb";
+import messages from "../packages/grpc/greeter_pb";
+
+(async () => {
+  console.log("Hello World!");
+
+  const server = new Server();
+
+  function sayHello(call: any, callback: any) {
+    const reply = new messages.HelloReply();
+    reply.setMessage("Hello " + call.request.getName());
+    callback(null, reply);
+  }
+
+  server.addService(services.GreeterService as any, {
+    sayHello,
+  });
+
+  server.bindAsync("localhost:6000", ServerCredentials.createInsecure(), () => {
+    server.start();
+  });
+})();
