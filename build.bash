30c2968dcfa8f40cde0151e5001e308171b8f2c0
dasoncheng <dasoncheng@outlook.com>
2021-10-20 22:25:55 +0800

    doc: 编译脚本添加

--- build.bash
+++ build.bash
@@ -1,11 +1,13 @@
 INPUT_DIR="./protos"
 OUT_DIR="./packages/grpc"
+PROTOC_GENG_RPC="./node_modules/.bin/grpc_tools_node_protoc_plugin"
+PROTOC_GEN_TS="./node_modules/.bin/protoc-gen-ts"
 
 npx grpc_tools_node_protoc \
 --proto_path="${INPUT_DIR}" \
 --js_out="import_style=commonjs,binary:${OUT_DIR}" \
 --grpc_out="grpc_js:${OUT_DIR}" \
---plugin="protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin" \
---plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+--plugin="protoc-gen-grpc=${PROTOC_GENG_RPC}" \
+--plugin="protoc-gen-ts=${PROTOC_GEN_TS}" \
 --ts_out="${OUT_DIR}" \
 ${INPUT_DIR}/*.proto
---
+++ build.ps1
@@ -0,0 +1,6 @@
+$INPUT_DIR=".\protos"
+$OUT_DIR=".\packages\grpc"
+$PROTOC_GENG_RPC=".\node_modules\.bin\grpc_tools_node_protoc_plugin.cmd"
+$PROTOC_GEN_TS=".\node_modules\.bin\protoc-gen-ts.cmd"
+
+npx grpc_tools_node_protoc --proto_path="$INPUT_DIR" --js_out="import_style=commonjs,binary:$OUT_DIR" --grpc_out="grpc_js:$OUT_DIR" --plugin="protoc-gen-grpc=$PROTOC_GENG_RPC" --plugin="protoc-gen-ts=$PROTOC_GEN_TS" --ts_out="$OUT_DIR" $INPUT_DIR\*.proto
